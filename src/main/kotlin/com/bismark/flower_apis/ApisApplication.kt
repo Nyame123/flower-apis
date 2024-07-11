f2a63854a541e592858ba0767c3f7bcb207f67fb
nyame <nyamebismark12.nb@gmail.com>
2024-07-11 23:15:51 +0000

    Started creating flower apis

---
+++ src/main/kotlin/com/bismark/flower_apis/ApisApplication.kt
@@ -0,0 +1,11 @@
+package com.bismark.flower_apis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApisApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApisApplication>(*args)
+}
---
+++ src/test/kotlin/com/bismark/flower_apis/ApisApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bismark.flower_apis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApisApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
